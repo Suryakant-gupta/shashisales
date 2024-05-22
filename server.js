7bc17d630d859e3ccc92bbba4c99fad268e5184b
Suryakant gupta <suryakantgupta678@gmail.com>
2024-05-22 14:33:40 +0530

    Your commit message

--- server.js
+++ server.js
@@ -89,6 +89,9 @@ app.get("/privacy-policy" , (req, res)=>{
     res.render("privacyPolicy")
 })
 
+app.get("/graphic-design" , (req, res)=>{
+    res.render("design")
+})
 
 //   const recipients = ['suryakantgupta678@gmail.com', 'bgmilelomujhse@gmail.com'];
   const recipients = ['anurag.tiwari@shashisales.com', 'info@shashisales.com'];
