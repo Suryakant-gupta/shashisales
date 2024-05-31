9cf8d45defe6a2df0d1c575487140ebe7e4c9aa0
Suryakant gupta <suryakantgupta678@gmail.com>
2024-05-31 18:06:10 +0530

    changes

--- public/js/new.js
+++ public/js/new.js
@@ -55,172 +55,159 @@ document.addEventListener('DOMContentLoaded', function() {
 // Design Page
 
 document.addEventListener('DOMContentLoaded', function() {
-  // Function to check if an element is in the viewport
-  function isInViewport(element) {
-      const rect = element.getBoundingClientRect();
-      return (
-          rect.top >= 0 &&
-          rect.left >= 0 &&
-          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
-  }
+    // Function to handle visibility check
+    function handleIntersection(entries, observer) {
+        entries.forEach(entry => {
+            const rocSec = entry.target;
+            if (entry.isIntersecting) {
+                rocSec.classList.add('active');
+                setTimeout(function() {
+                    rocSec.classList.add('reset');
+                }, 1300);
+            } else {
+                rocSec.classList.remove('active');
+                rocSec.classList.remove('reset');
+            }
+        });
+    }
 
-  // Function to handle visibility check
-  function checkVisibility() {
-      const rocSec = document.getElementById('roc-sec');
-      if (isInViewport(rocSec)) {
-          rocSec.classList.add('active');
-          setTimeout(function() {
-              rocSec.classList.add('reset');
-          }, 1300);
-      } else {
-          rocSec.classList.remove('active');
-          rocSec.classList.remove('reset');
-      }
-  }
+    // Set up Intersection Observer
+    const observer = new IntersectionObserver(handleIntersection, {
+        threshold: [0.7] // Trigger when at least 80% of the element is in view
+    });
 
-  // Initial check in case the element is already in view on page load
-  checkVisibility();
+    const rocSec = document.getElementById('roc-sec');
+    observer.observe(rocSec);
 
-  // Event listeners for scroll and resize
-  window.addEventListener('scroll', checkVisibility);
-  window.addEventListener('resize', checkVisibility);
+    // Throttle function to limit the rate of calls to a function
+    function throttle(fn, wait) {
+        let time = Date.now();
+        return function() {
+            if ((time + wait - Date.now()) < 0) {
+                fn();
+                time = Date.now();
+            }
+        }
+    }
 
-  // Variables and event listeners for mobile-specific behavior
-  let isHovered = false;
-  const forMob = document.querySelector('.for-mob');
-  const forMobSection = document.querySelector('.design-section-two');
-  const tblElements = document.querySelectorAll('.tbl');
+    // For Mobile - Design-section-two
+    const forMobSection = document.querySelector('.design-section-two');
+    const tblElements = document.querySelectorAll('.tbl');
 
-  function handleMobileScroll() {
-      if (window.innerWidth <= 768 && !isHovered) {
-          if (window.scrollY > 0) {
-              forMobSection.style.backgroundColor = '#000';
-          } else {
-              forMobSection.style.backgroundColor = 'transparent';
-          }
-      }
-  }
+    function isElementInCenter(element) {
+        const rect = element.getBoundingClientRect();
+        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+        const elementCenter = rect.top + rect.height / 2;
+        return elementCenter >= windowHeight / 3 && elementCenter <= (2 * windowHeight) / 3;
+    }
 
-  function handleMobileTblScroll() {
-      if (window.innerWidth <= 768) {
-          if (window.scrollY > 0) {
-              forMobSection.style.backgroundColor = '#000';
-              tblElements.forEach(function(tblElement) {
-                  tblElement.classList.add('white-text');
-              });
-          } else {
-              forMobSection.style.backgroundColor = 'transparent';
-              tblElements.forEach(function(tblElement) {
-                  tblElement.classList.remove('white-text');
-              });
-          }
-      }
-  }
+    function handleScrollEffect() {
+        if (window.innerWidth <= 768) {
+            if (isElementInCenter(forMobSection)) {
+                forMobSection.style.backgroundColor = 'white';
+                tblElements.forEach(function(tblElement) {
+                    tblElement.classList.remove('white-text');
+                });
+            } else {
+                forMobSection.style.backgroundColor = 'black';
+                tblElements.forEach(function(tblElement) {
+                    tblElement.classList.add('white-text');
+                });
+            }
+        }
+    }
 
-  // Event listeners for scroll and hover effects
-  window.addEventListener('scroll', handleMobileScroll);
-  window.addEventListener('scroll', handleMobileTblScroll);
+    window.addEventListener('scroll', handleScrollEffect);
+    window.addEventListener('resize', handleScrollEffect);
 
-  forMob.addEventListener('mouseenter', function() {
-      isHovered = true;
-      forMob.style.backgroundColor = '#000';
-  });
 
-  forMob.addEventListener('mouseleave', function() {
-      isHovered = false;
-      if (window.scrollY === 0) {
-          forMob.style.backgroundColor = 'transparent';
-      }
-  });
+    handleScrollEffect();
 
-  // Rocket animation specific behavior for mobile
-  if (window.innerWidth <= 600) {
-      var rocket = document.getElementById("rocket");
-      var rocketImage = rocket.querySelector("img");
+    // Rocket animation for mobile
+    if (window.innerWidth <= 600) {
+        var rocket = document.getElementById("rocket");
+        var rocketImage = rocket.querySelector("img");
 
-      rocket.addEventListener("animationstart", function() {
-          rocketImage.src = "/assets/images/rocket.png";
-      });
+        rocket.addEventListener("animationstart", function() {
+            rocketImage.src = "/assets/images/rocket.png";
+        });
 
-      rocket.addEventListener("animationiteration", function() {
-          rocketImage.src = "/assets/images/rocket.png";
-      });
+        rocket.addEventListener("animationiteration", function() {
+            rocketImage.src = "/assets/images/rocket.png";
+        });
 
-      rocket.addEventListener("animationstart", function(e) {
-          var duration = e.target.getAnimations()[0].effect.getTiming().duration;
+        rocket.addEventListener("animationstart", function(e) {
+            var duration = e.target.getAnimations()[0].effect.getTiming().duration;
 
-          setTimeout(function() {
-              rocketImage.src = "/assets/images/rocket2.png";
-          }, 0.1220 * duration);
+            setTimeout(function() {
+                rocketImage.src = "/assets/images/rocket2.png";
+            }, 0.1220 * duration);
 
-          setTimeout(function() {
-              rocketImage.src = "/assets/images/rocket.png";
-          }, 0.3333 * duration);
+            setTimeout(function() {
+                rocketImage.src = "/assets/images/rocket.png";
+            }, 0.3333 * duration);
 
-          setTimeout(function() {
-              rocketImage.src = "/assets/images/rocket2.png";
-          }, 0.6667 * duration);
+            setTimeout(function() {
+                rocketImage.src = "/assets/images/rocket2.png";
+            }, 0.6667 * duration);
 
-          setTimeout(function() {
-              rocketImage.src = "/assets/images/rocket.png";
-          }, duration);
-      });
-  }
+            setTimeout(function() {
+                rocketImage.src = "/assets/images/rocket.png";
+            }, duration);
+        });
+    }
 
-  // Hover effect for images
-  function addHoverEffect(elementId, originalSrc, hoverSrc) {
-      const element = document.getElementById(elementId).querySelector('img');
-      element.addEventListener('mouseenter', function() {
-          element.src = hoverSrc;
-      });
-      element.addEventListener('mouseleave', function() {
-          element.src = originalSrc;
-      });
-  }
+    // Hover effect for images
+    function addHoverEffect(elementId, originalSrc, hoverSrc) {
+        const element = document.getElementById(elementId).querySelector('img');
+        element.addEventListener('mouseenter', function() {
+            element.src = hoverSrc;
+        });
+        element.addEventListener('mouseleave', function() {
+            element.src = originalSrc;
+        });
+    }
 
-  addHoverEffect('ar1', '/assets/images/ar1.png', '/assets/images/ar11.jpeg');
-  addHoverEffect('ar2', '/assets/images/ar2.png', '/assets/images/ar22.jpeg');
-  addHoverEffect('ar3', '/assets/images/ar3.png', '/assets/images/ar33.jpeg');
-  addHoverEffect('ar4', '/assets/images/ar4.png', '/assets/images/ar44.jpeg');
+    addHoverEffect('ar1', '/assets/images/ar1.png', '/assets/images/ar11.jpeg');
+    addHoverEffect('ar2', '/assets/images/ar2.png', '/assets/images/ar22.jpeg');
+    addHoverEffect('ar3', '/assets/images/ar3.png', '/assets/images/ar33.jpeg');
+    addHoverEffect('ar4', '/assets/images/ar4.png', '/assets/images/ar44.jpeg');
 
-  // Scroll and hover effect for animated elements
-  const animatedElements = document.querySelectorAll('.animate-on-scroll');
+    const animatedElements = document.querySelectorAll('.animate-on-scroll');
 
-  function handleScroll() {
-      animatedElements.forEach(element => {
-          const rect = element.getBoundingClientRect();
-          const isInViewport = rect.top < window.innerHeight && rect.bottom > 0;
+    function handleScroll() {
+        animatedElements.forEach(element => {
+            const rect = element.getBoundingClientRect();
+            const isInViewport = rect.top < window.innerHeight && rect.bottom > 0;
 
-          if (isInViewport) {
-              element.classList.add('scrolled');
-          } else {
-              element.classList.remove('scrolled');
-          }
-      });
-  }
+            if (isInViewport) {
+                element.classList.add('scrolled');
+            } else {
+                element.classList.remove('scrolled');
+            }
+        });
+    }
 
-  // Event listeners for scroll and load
-  window.addEventListener('scroll', handleScroll);
-  window.addEventListener('load', handleScroll);
+    window.addEventListener('scroll', throttle(handleScroll, 100));
+    window.addEventListener('load', handleScroll);
 
-  // Hover effect for animated elements
-  animatedElements.forEach(element => {
-      element.addEventListener('mouseenter', () => {
-          element.classList.add('scrolled');
-      });
 
-      element.addEventListener('mouseleave', () => {
-          const rect = element.getBoundingClientRect();
-          const isInViewport = rect.top < window.innerHeight && rect.bottom > 0;
+    animatedElements.forEach(element => {
+        element.addEventListener('mouseenter', () => {
+            element.classList.add('scrolled');
+        });
 
-          if (!isInViewport) {
-              element.classList.remove('scrolled');
-          }
-      });
-  });
+        element.addEventListener('mouseleave', () => {
+            const rect = element.getBoundingClientRect();
+            const isInViewport = rect.top < window.innerHeight && rect.bottom > 0;
+
+            if (!isInViewport) {
+                element.classList.remove('scrolled');
+            }
+        });
+    });
 });
+
 
 
 
