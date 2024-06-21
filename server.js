2215e753cb49fbb03b136c3da791deaac1942703
Suryakant gupta <suryakantgupta678@gmail.com>
2024-06-21 16:56:15 +0530

    changes

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const crypto = require('crypto');
 
 const dotenv = require('dotenv');
 const mailsender = require("./utils/mailsender");
+const Templatesender = require("./utils/templatemailer");
 const session = require('express-session');
 const flash = require('connect-flash');
 const mongoose = require("mongoose");
@@ -473,7 +474,7 @@ app.get("/phonepe-form", async (req, res) => {
 
 app.post("/payment", async (req, res) => {
     try {
-        const { name, number, amount } = req.body;
+        const { name, number, amount, email } = req.body;
         const merchantTransactionId =  'T' + Date.now();;
         const data = {
             "merchantId": process.env.PHONEPE_MERCHANT_ID,
@@ -516,6 +517,338 @@ app.post("/payment", async (req, res) => {
   .request(options)
       .then(function (response) {
           console.log(response.data);
+
+          const htmlTemplate =`
+          <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shashi sales and marketing</title>
+
+    <style>
+        ._failed {
+            border-bottom: solid 4px red !important;
+        }
+
+        ._failed i {
+            color: red !important;
+        }
+
+        .bl {
+            background-color: black;
+        }
+
+        .container {
+            width: 100%;
+            margin-top: 6rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+
+        ._success {
+            box-shadow: 0 15px 25px #00000019;
+            padding: 45px;
+            width: 100%;
+            text-align: center;
+            margin: 40px auto;
+            border-bottom: solid 4px #28a745;
+        }
+
+        ._success i {
+            font-size: 55px;
+            color: #28a745;
+        }
+
+        ._success h2 {
+            margin-bottom: 12px;
+            font-size: 40px;
+            font-weight: 500;
+            line-height: 1.2;
+            margin-top: 10px;
+        }
+
+        ._success p {
+            margin-bottom: 20px;
+            font-size: 18px;
+            color: #495057;
+            font-weight: 500;
+        }
+        a{
+            color: blue;
+            font-size: 1.2rem;
+            font-weight: bold;
+            text-decoration: underline;
+           
+        }
+        h3{
+            font-size: 1.5rem;
+            margin-bottom: 20px;
+        }
+        span{
+            font-size: 2rem;
+            color: #28a745;
+        }
+    </style>
+</head>
+<body>
+    <style>
+        ._failed {
+            border-bottom: solid 4px red !important;
+        }
+
+        ._failed i {
+            color: red !important;
+        }
+
+        .bl {
+            background-color: black;
+        }
+
+        .container {
+            width: 100%;
+            margin-top: 6rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+
+        ._success {
+            box-shadow: 0 15px 25px #00000019;
+            padding: 45px;
+            width: 100%;
+            text-align: center;
+            margin: 40px auto;
+            border-bottom: solid 4px #28a745;
+        }
+
+        ._success i {
+            font-size: 55px;
+            color: #28a745;
+        }
+
+        ._success h2 {
+            margin-bottom: 12px;
+            font-size: 40px;
+            font-weight: 500;
+            line-height: 1.2;
+            margin-top: 10px;
+        }
+
+        ._success p {
+            margin-bottom: 20px;
+            font-size: 18px;
+            color: #495057;
+            font-weight: 500;
+        }
+        a{
+            color: blue;
+            font-size: 1.2rem;
+            font-weight: bold;
+            text-decoration: underline;
+           
+        }
+        h3{
+            font-size: 1.5rem;
+            margin-bottom: 20px;
+        }
+        span{
+            font-size: 2rem;
+            color: #28a745;
+        }
+    </style>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="message-box _success">
+                    <i class="fa fa-check-circle" aria-hidden="true"></i>
+                    <h2> Your payment was successful </h2>
+                    <h3>Amount paid: <span>₹${amount}</span></h3>
+                    <p> Thank you ${name} for your payment. we will <br>
+                        be in contact with more details shortly </p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</body>
+</html>
+          `
+
+          Templatesender(email, htmlTemplate);
+
+
+          const htmlTemplate2 =`
+          <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shashi sales and marketing</title>
+
+    <style>
+        ._failed {
+            border-bottom: solid 4px red !important;
+        }
+
+        ._failed i {
+            color: red !important;
+        }
+
+        .bl {
+            background-color: black;
+        }
+
+        .container {
+            width: 100%;
+            margin-top: 6rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+
+        ._success {
+            box-shadow: 0 15px 25px #00000019;
+            padding: 45px;
+            width: 100%;
+            text-align: center;
+            margin: 40px auto;
+            border-bottom: solid 4px #28a745;
+        }
+
+        ._success i {
+            font-size: 55px;
+            color: #28a745;
+        }
+
+        ._success h2 {
+            margin-bottom: 12px;
+            font-size: 40px;
+            font-weight: 500;
+            line-height: 1.2;
+            margin-top: 10px;
+        }
+
+        ._success p {
+            margin-bottom: 20px;
+            font-size: 18px;
+            color: #495057;
+            font-weight: 500;
+        }
+        a{
+            color: blue;
+            font-size: 1.2rem;
+            font-weight: bold;
+            text-decoration: underline;
+           
+        }
+        h3{
+            font-size: 1.5rem;
+            margin-bottom: 20px;
+        }
+        span{
+            font-size: 2rem;
+            color: #28a745;
+        }
+    </style>
+</head>
+<body>
+    <style>
+        ._failed {
+            border-bottom: solid 4px red !important;
+        }
+
+        ._failed i {
+            color: red !important;
+        }
+
+        .bl {
+            background-color: black;
+        }
+
+        .container {
+            width: 100%;
+            margin-top: 6rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+
+        ._success {
+            box-shadow: 0 15px 25px #00000019;
+            padding: 45px;
+            width: 100%;
+            text-align: center;
+            margin: 40px auto;
+            border-bottom: solid 4px #28a745;
+        }
+
+        ._success i {
+            font-size: 55px;
+            color: #28a745;
+        }
+
+        ._success h2 {
+            margin-bottom: 12px;
+            font-size: 40px;
+            font-weight: 500;
+            line-height: 1.2;
+            margin-top: 10px;
+        }
+
+        ._success p {
+            margin-bottom: 20px;
+            font-size: 18px;
+            color: #495057;
+            font-weight: 500;
+        }
+        a{
+            color: blue;
+            font-size: 1.2rem;
+            font-weight: bold;
+            text-decoration: underline;
+           
+        }
+        h3{
+            font-size: 1.5rem;
+            margin-bottom: 20px;
+        }
+        span{
+            font-size: 2rem;
+            color: #28a745;
+        }
+    </style>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="message-box _success">
+                    <i class="fa fa-check-circle" aria-hidden="true"></i>
+                    <h2> You got payment from ${name} </h2>
+                    <h3>Amount paid: <span>₹${amount}</span></h3>
+                    <p> User detail : 
+                    </p>
+                    <p>Name : ${name} </p>
+                    <p>Number : ${number} </p>
+                    <p>Email : ${email}</p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</body>
+</html>
+          `
+          Templatesender("info@shashisales.com" , htmlTemplate2)
+
         res.redirect(response.data.data.instrumentResponse.redirectInfo.url)
 
   })
@@ -596,6 +929,8 @@ app.post("/status/:txnId", async (req, res) => {
 app.get("/payment-successful", (req, res) => {
     const amount = req.query.amount || 'N/A';
     res.render("paymentsucess.ejs", { amount: amount });
+
+   
 });
 app.get("/payment-failed" , (req, res)=>{
     res.render("paymentfail.ejs")
---
+++ utils/templatemailer.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer');
+
+const dotenv = require('dotenv');
+
+
+// Load environment variables if using .env
+dotenv.config();
+
+const fromUser = process.env.FROM_EMAIL;
+const password = process.env.EMAIL_PASS;
+
+
+// Create a reusable transporter object
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: fromUser, 
+    pass: password
+  }
+});
+
+// Function to send email notification
+const sendEmail = (recipient, htmlTemplate) => {
+  const mailOptions = {
+    from: fromUser, // Replace with your Gmail email
+    to: recipient,
+    subject: 'New Form Submission',
+    html: htmlTemplate,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+    } else {
+      console.log("Email sent:", info.response);
+    }
+  });
+};
+
+module.exports = sendEmail;
