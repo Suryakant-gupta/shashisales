3059fc503b14b87ad9fe2889a04dbe44b1b25378
Suryakant gupta <suryakantgupta678@gmail.com>
2024-07-05 14:33:11 +0530

    changes

--- server.js
+++ server.js
@@ -1311,8 +1311,8 @@ app.post("/status/:txnId", async (req, res) => {
 
 app.get("/payment-successful", (req, res) => {
     const amount = req.query.amount || 'N/A';
-    res.render("paymentsucess.ejs", { amount: amount , title: " ",
-    description: " " });
+    res.render("paymentsucess.ejs", { amount: amount , title: "hello",
+    description: "hello" });
 
 
 });
